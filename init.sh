d20ca09e652ab5cc08eda20dacc8ba1fe701174e
Brian Hartford <bhartford419@gmail.com>
2022-11-26 20:39:12 -0500

    Add -p flag to useradd

--- init.sh
+++ init.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 echo "Please enter the username you could like to create and press ENTER: "
-read username
+read USERNAME
 echo "Enter user password: "
-read passwd
+read PASSWD
 #create user, prompt for password, add to sudo group
-sudo useradd -m ${username} -p ${passwd}# -D -s /bin/bash
-usermod -aG sudo ${username}
+#sudo useradd -m ${username} -p ${passwd}# -D -s /bin/bash
+#usermod -aG sudo ${username}
+
+sudo useradd -m -u $USERNAME  -o -d /home/$USERNAME -s /bin/bash $USERNAME && \
+usermod -aG sudo $USERNAME
+
 
 echo -e "Do you want to sign in as this user now?\n(yes|no)\n"
 read answer
 if [ $answer == yes ]; then
-    su -${username}
+    su $USERNAME
 else
     echo "user created....exiting"
 fi
